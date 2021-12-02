265b291910cd697d3b1dee266bd0130f42dbbd4f
alexproudmur <alexproudmur@gmail.com>
2021-12-02 23:56:31 +0200

    Logic fix

--- Lab5/src/com/company/player/AI.java
+++ Lab5/src/com/company/player/AI.java
@@ -14,6 +14,14 @@ public class AI implements Player {
         GameTree gameTree = new GameTree(field, Game.depth);
         minimax.minimax(gameTree.root);
         int holeToMove = minimax.stack.pop().movedHole;
+        if (field.holes[holeToMove-1].count == 0) {
+            for (int i = 5; i < 9; i++) {
+                if (field.holes[i].count != 0) {
+                    holeToMove = i + 1;
+                    break;
+                }
+            }
+        }
         return field.move(holeToMove, false);
     }
 
