03c3da3550f7fd16bdee91a653483a438cfd6e89
alexproudmur <alexproudmur@gmail.com>
2021-12-03 12:54:49 +0200

    Interface improvement

--- Lab5/src/com/company/Main.java
+++ Lab5/src/com/company/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Insert difficulty: Low, Medium, High");
         Scanner scanner = new Scanner(System.in);
         String difficulty = scanner.next();
         Game game = new Game(difficulty);
